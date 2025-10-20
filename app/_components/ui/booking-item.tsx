c7c0f370925673dba1738021a57b938ac7b50c0e
Jairo Lima <jairox.lima#gmail.com>
2025-10-20 08:29:36 -0300

    feat: add BookingItem component

---
+++ app/_components/ui/booking-item.tsx
@@ -0,0 +1,38 @@
+import { Avatar, AvatarImage } from "./avatar"
+import { Badge } from "./badge"
+import { Card, CardContent } from "./card"
+
+const BookingItem = () => {
+  return (
+    <>
+      <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+        Agendamentos
+      </h2>
+
+      <Card>
+        <CardContent className="flex justify-between p-0">
+          {/*ESQUERDA*/}
+          <div className="flex flex-col gap-2 py-5 pl-5">
+            <Badge className="w-fit">Confirmado</Badge>
+            <h3 className="font-semibold">Corte de Cabelo</h3>
+
+            <div className="flex items-center gap-2">
+              <Avatar className="h-6 w-6">
+                <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+              </Avatar>
+              <p className="text-sm">Barbearia FSW</p>
+            </div>
+          </div>
+          {/*DIREITA*/}
+          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-10">
+            <p className="text-sm">Outubro</p>
+            <p className="text-2xl">17</p>
+            <p className="text-sm">8:15 - AM</p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  )
+}
+
+export default BookingItem
--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,29 @@ import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/ui/booking-item"
+
+// TODO: receber agendamento com prop
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
 
   return (
     <div>
       <Header />
-      <div className="p-6">
+      <div className="p-5">
+        {/*TEXTO*/}
         <h2 className="text-xl font-bold">Olá, Jairo!</h2>
         <p>terça-feira, 14 de outubro.</p>
-
+        {/*BUSCA*/}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
           <Button>
@@ -26,6 +34,22 @@ const Home = async () => {
           </Button>
         </div>
 
+        {/*BUSCA RÁPIDA*/}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image
+                src={option.imageUrl}
+                width={16}
+                height={16}
+                alt={option.title}
+              />
+              {option.title}
+            </Button>
+          ))}
+        </div>
+
+        {/*IMAGE*/}
         <div className="relative mt-6 h-[150px] w-full overflow-hidden rounded-xl">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -34,31 +58,8 @@ const Home = async () => {
             className="object-cover"
           />
         </div>
-
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-10">
-              <p className="text-sm">Outubro</p>
-              <p className="text-2xl">17</p>
-              <p className="text-sm">8:15 - AM</p>
-            </div>
-          </CardContent>
-        </Card>
+        {/*AGEMGAMENTO*/}
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
@@ -69,8 +70,24 @@ const Home = async () => {
           ))}
         </div>
 
-        <div></div>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6 ">
+            <p className="text-sm text-gray-400">
+              @ 2025 Copyright <span className="font-bold"> Barber </span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
