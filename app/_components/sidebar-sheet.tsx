2eb99a1d9cea6f9065423ccbdd9d2595b842e9e2
Jairo Lima <jairox.lima#gmail.com>
2025-10-28 14:43:43 -0300

    feat: add SidebarSheet component

---
+++ app/_components/sidebar-sheet.tsx
@@ -0,0 +1,71 @@
+import { Button } from "./ui/button"
+import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
+import { quickSearchOptions } from "../_constants/search"
+import { Avatar } from "./ui/avatar"
+import { AvatarImage } from "@radix-ui/react-avatar"
+import Link from "next/link"
+import Image from "next/image"
+
+const SidebarSheet = () => {
+  return (
+    <SheetContent className="overflow-y-auto">
+      <SheetHeader>
+        <SheetTitle className="text-left">Menu</SheetTitle>
+      </SheetHeader>
+
+      <div className="flex items-center border-b border-solid py-5">
+        <Avatar>
+          <AvatarImage src="https://images.unsplash.com/photo-1714484368318-c26a8df5904a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=483" />
+        </Avatar>
+        <div>
+          <p className="font-bold">Jairo Lima</p>
+          <p className="text-xs">jairox.lima@gmail.com</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
+        <SheetClose asChild>
+          <Button className="justify-start gap-2" variant="ghost" asChild>
+            <Link href="/">
+              <HomeIcon size={18} />
+              Inicio
+            </Link>
+          </Button>
+        </SheetClose>
+        <Button className="justify-start gap-2" variant="ghost">
+          <CalendarIcon size={18} />
+          Agendamento
+        </Button>
+      </div>
+
+      <div className="flex flex-col gap-2 border-b border-solid py-5">
+        {quickSearchOptions.map((option) => (
+          <Button
+            key={option.title}
+            className="justify-start gap-2"
+            variant="ghost"
+          >
+            <HomeIcon size={18} />
+            <Image
+              src={option.imageUrl}
+              height={18}
+              width={18}
+              alt={option.title}
+            />
+            {option.title}
+          </Button>
+        ))}
+      </div>
+
+      <div className="flex flex-col gap-2 py-5">
+        <Button variant="ghost" className="justify-start gap-2">
+          <LogOutIcon size={18} />
+          Sair da conta
+        </Button>
+      </div>
+    </SheetContent>
+  )
+}
+
+export default SidebarSheet
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -6,6 +6,8 @@ import { db } from "@/app/_lib/prisma"
 import { notFound } from "next/navigation"
 import ServicesItem from "@/app/_components/services.item"
 import PhoneItem from "@/app/_components/phone-item"
+import { Sheet, SheetTrigger } from "@/app/_components/ui/sheet"
+import SidebarSheet from "@/app/_components/sidebar-sheet"
 
 interface BarbershopPageProps {
   params: {
@@ -48,13 +50,18 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           </Link>
         </Button>
 
-        <Button
-          size="icon"
-          variant="secondary"
-          className="absolute right-4 top-4"
-        >
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              size="icon"
+              variant="outline"
+              className="absolute right-4 top-4"
+            >
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SidebarSheet />
+        </Sheet>
       </div>
 
       {/* DADOS */}
