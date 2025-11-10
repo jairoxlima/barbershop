13728bd6a992333a4e6f76e6ac9ff49b9b981fcc
Jairo Lima <jairox.lima#gmail.com>
2025-11-10 11:43:07 -0300

    feat: ensure only signed in user can book and sign out

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -6,20 +6,13 @@ import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
 import Link from "next/link"
 import Image from "next/image"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "./ui/dialog"
-import { signIn, signOut, useSession } from "next-auth/react"
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
+import { signOut, useSession } from "next-auth/react"
 import { Avatar, AvatarImage } from "./ui/avatar"
+import SingInDialog from "./sign-in-dialog"
 
 const SidebarSheet = () => {
   const { data } = useSession()
-  const handleLoginWithGoogleClick = () => signIn("google")
   const handleLogoutClick = () => signOut()
 
   console.log(data?.user)
@@ -55,26 +48,8 @@ const SidebarSheet = () => {
                   <LogInIcon />
                 </Button>
               </DialogTrigger>
-              <DialogContent className="w-[90%]">
-                <DialogHeader>
-                  <DialogTitle>Faça login na platafroma</DialogTitle>
-                  <DialogDescription>
-                    Conecte-se usando sua conta do Google.
-                  </DialogDescription>
-                </DialogHeader>
-                <Button
-                  variant="outline"
-                  className="gap-1 font-bold"
-                  onClick={handleLoginWithGoogleClick}
-                >
-                  <Image
-                    src="/google.svg"
-                    width={18}
-                    height={18}
-                    alt="Fazer login com o Google"
-                  />
-                  Google
-                </Button>
+              <DialogContent className="w-[90%">
+                <SingInDialog />
               </DialogContent>
             </Dialog>
           </>
@@ -114,16 +89,18 @@ const SidebarSheet = () => {
         ))}
       </div>
 
-      <div className="flex flex-col gap-2 py-5">
-        <Button
-          variant="ghost"
-          className="justify-start gap-2"
-          onClick={handleLogoutClick}
-        >
-          <LogOutIcon size={18} />
-          Sair da conta
-        </Button>
-      </div>
+      {data?.user && (
+        <div className="flex flex-col gap-2 py-5">
+          <Button
+            variant="ghost"
+            className="justify-start gap-2"
+            onClick={handleLogoutClick}
+          >
+            <LogOutIcon size={18} />
+            Sair da conta
+          </Button>
+        </div>
+      )}
     </SheetContent>
   )
 }
---
+++ app/_components/sign-in-dialog.tsx
@@ -0,0 +1,34 @@
+import { signIn } from "next-auth/react"
+import { Button } from "./ui/button"
+import { DialogDescription, DialogHeader, DialogTitle } from "./ui/dialog"
+import Image from "next/image"
+
+const SingInDialog = () => {
+  const handleLoginWithGoogleClick = () => signIn("google")
+
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>Faça login na platafroma</DialogTitle>
+        <DialogDescription>
+          Conecte-se usando sua conta do Google.
+        </DialogDescription>
+      </DialogHeader>
+      <Button
+        variant="outline"
+        className="gap-1 font-bold"
+        onClick={handleLoginWithGoogleClick}
+      >
+        <Image
+          src="/google.svg"
+          width={18}
+          height={18}
+          alt="Fazer login com o Google"
+        />
+        Google
+      </Button>
+    </>
+  )
+}
+
+export default SingInDialog
